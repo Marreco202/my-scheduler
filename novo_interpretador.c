9121d38d6366809923d7a4c77f8c2d10f49f62a3
João Victor Godinho W <76889373+Marreco202@users.noreply.github.com>
2022-10-14 11:42:21 -0300

    rascunho

--- novo_interpretador.c
+++ novo_interpretador.c
@@ -5,8 +5,8 @@
 #include <sys/wait.h>
 
 /*
-Beatriz Sordero - 
 João Victor Godinho Woitschach - 2011401
+Maria Beatriz Sobreira - 2010953
 */
 
 /*
@@ -103,6 +103,53 @@ int get_tempoExecTotal(Fila* f){
     return tempo_total;
 }
 
+El* next_off(Fila* f, int tempo_atual){//TESTAR PARA VER SE FUNCIONA || seleciona o proximo a sair da LL
+    
+    while(f!=NULL){
+        if(f->curr->tempo_init == tempo_atual)
+            return f->curr;
+        f = f->prox;
+    }
+
+    return NULL; //nao foi encontrado elemento a ser inserido
+}
+
+Fila* remove_of_line(Fila*f , El* target){//TESTAR || remove da fila o elemento target, e retorna uma nova fila sem aquele elemento
+
+    Fila* ant = NULL; //referencia ao ponteiro para o anterior
+    Fila* buf = f;
+
+    while(buf!=NULL){
+        if(buf->curr == target){
+            if(ant == NULL){ //o elemento a ser retirado é o primeiro da LL
+                f = f->prox;
+                free(buf->curr);
+                free(buf);
+                return f;
+            }else{
+                ant->prox = buf->prox; // skip 1
+                free(buf->curr);
+                free(buf);
+                return f;
+            }
+        }
+        ant = buf;
+        buf = buf->prox;
+    }
+    return f; //nao foi encontrado o elemento target
+}
+
+int get_line_size(Fila *f){
+    int tam = 0;
+    while(f!=NULL){
+        tam++;
+        f = f->prox;
+    }
+
+    return tam;
+}
+
+
 void free_line(Fila* f){
     
     Fila *ref; //referencia para dar free
@@ -130,7 +177,7 @@ int main(void){
     
     int lever = 0; //booleana que verifica se o primeiro elemento ja foi inserido
 
-    while(!feof(f)){
+    while(!feof(f)){ /*      LEITURA DO ARQUIVO       */
         
         int name, prio, tempo_init, tempo_total;
         fscanf(f,"exec p%d, prioridade=%d, inicio_tempo_execucao=%d, tempo_total_ execucao =%d\n",&name,&prio,&tempo_init,&tempo_total);
@@ -146,33 +193,60 @@ int main(void){
         
     }
 
+    /*          INICIO DO PROGRAMA            */
+
     int tempoTotalExec = get_tempoExecTotal(fila); //tempo total de execucao de todos os programas, quando esse tempo acabar, finalizar escalonador e o programa 
     int *runTime = (int*) malloc(sizeof(int)); //ha quanto tempo o programa esta rodando
     *runTime = 0;
+    int tam  = get_line_size(fila); //tamanho total da linked list, e da quantidade de elementos possiveis a serem passados por parametro para o escalonador
+    lever = 0; //booleana para verificar se ja ha pelo menos 1 elemento na RR
+
+    El* target = NULL; //alvo  a ser retirado da LL e inserido da RR
+    Fila *RR; // lista round-robin, com todos os processos na fila de prontos no momento
 
     printf("tempo total de execucao: %d\n!!!!!!!!!!!\n",tempoTotalExec);
 
 
-    /* TIRAR ESSE COMENTARIO
+    // TIRAR ESSE COMENTARIO
 
     int escalonador = fork();
 
+    El* next; //elemento a ser inserido neste segundo na lista
+
     if(escalonador == 0){ //interpretador
         while(runTime != tempoTotalExec){ //CADA ITERACAO REPRESENTA 1 SEGUNDO!!!
+            while(next_off(fila,runTime) != NULL){//enquanto houverem elementos a serem inseridos neste segundo:
+                target = next_off(fila,runTime); //pega o elemento alvo
+                fila = remove_of_line(fila,target); //remove ele da LL
+
+                if(lever == 0){//se nao houver nenhum elemento na RR;
+                    RR = create_line(target);
+                }else{ //se houver ao menos um elemento na RR
+                    insert_on_line(RR,target);
+                }
+            }
 
             sleep(1);
 
-            *runTime++;
+            *runTime++;//+1 segundo
         }
-        //faz chamadas ao escalonador
+        /*      faz chamadas ao escalonador         */
+
+        
+
+
+        kill(escalonador,SIGCONT); //isso talvez de erro, 
+
     }else if(escalonador > 0){ //escalonador
-        //waitpid para esperar o pai mandar o SIGCONT
+        int mypid = getpid();
+
+        //waitpid para esperar o pai mandar o SIGCONT para o PID do escalonador
         sleep(1);
     }
 
     //faz fork, e faz chamadas ao escalonador para ele inserir na paradinha do lance
 
-    TIRAR ESSE COMENTARIO */
+    //TIRAR ESSE COMENTARIO 
 
     print_line(fila);
 
